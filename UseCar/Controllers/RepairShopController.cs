773e527e99fe9f620c14bc8ca476326b80de7a48
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-01 19:56:30 +0700

    เพิ่มส่วนจัดการข้อมูลร้านซ่อม

---
+++ UseCar/Controllers/RepairShopController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UseCar.Controllers
+{
+    public class RepairShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult CategoryShop()
+        {
+            return View();
+        }
+    }
+}
