bbf55dc37517ad62f007b4998b5a7d4addea33df
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-09 15:18:34 +0700

    เพิ่มหน้าจัดการข้อมูลผู้ใช้งาน

---
+++ UseCar/Controllers/UserManagementController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UseCar.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
