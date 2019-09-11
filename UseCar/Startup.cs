0071658b25fab59093792d42be3f3aa4bdb79461
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-11 21:22:00 +0700

    inject dbContext

--- UseCar/Startup.cs
+++ UseCar/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using UseCar.Models;
 
 namespace Car_Somchai
 {
@@ -30,6 +32,8 @@ namespace Car_Somchai
             //    options.CheckConsentNeeded = context => true;
             //    options.MinimumSameSitePolicy = SameSiteMode.None;
             //});
+            services.AddDbContext<UseCarDBContext>(options =>
+        options.UseMySql(Configuration.GetConnectionString("UseCarContext")));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
