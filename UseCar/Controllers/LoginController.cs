ca5eb4047da0e0b0640913f1d253c2bf26b0ce5f
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-15 21:57:08 +0700

    check for login

--- UseCar/Controllers/LoginController.cs
+++ UseCar/Controllers/LoginController.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Car_Somchai.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using UseCar.Helper;
+using UseCar.Models;
 
 namespace Car_Somchai.Controllers
 {
     public class LoginController : Controller
     {
+        readonly UseCarDBContext context;
+        public LoginController(UseCarDBContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
             return View(new LoginViewModel());
@@ -16,7 +23,38 @@ namespace Car_Somchai.Controllers
         [HttpPost]
         public IActionResult Login(LoginViewModel data)
         {
-            return Redirect("/Home");
+            var user = (from a in context.user
+                        where a.isEnable
+                        && a.userName == data.Username
+                        && GeneratePassword.PasswordCheck(data.Password, a.salt, a.password)
+                        select a).FirstOrDefault();
+            if (user == null)
+            {
+                return Json(new ResponseResult
+                {
+                    code = ResponseCode.error,
+                    message = "Username or Password Incorrect!"
+                });
+            }
+            else
+            {
+                if (!user.isActive)
+                {
+                    return Json(new ResponseResult
+                    {
+                        code = ResponseCode.error,
+                        message = "Username InActive"
+                    });
+                }
+                else
+                {
+                    return Json(new ResponseResult
+                    {
+                        code = ResponseCode.ok,
+                        message = "/Home"
+                    });
+                }
+            }
         }
     }
 }
