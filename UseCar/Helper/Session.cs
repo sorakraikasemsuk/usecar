cd03ce5063f9b090a139377ca89cac44c4c679dc
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-15 22:06:50 +0700

    add helper session

---
+++ UseCar/Helper/Session.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseCar.Helper
+{
+    public static class Session
+    {
+        public const string userId = "userId";
+    }
+}
