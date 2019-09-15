756d9bf4a094c9ed0486f7dd3d56efa5a3ab9307
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-15 14:44:41 +0700

    add function ResponseDialog

--- UseCar/wwwroot/js/site.js
+++ UseCar/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function ResponseDialog(code,location) {
+    if (code === 200) {
+        $.showAlert({
+            title: "แจ้งเตือน",
+            body: "บันทึกข้อมูลสำเร็จ",
+            onDispose: function () {
+                window.location.href = location;
+            }
+        });
+    } else {
+        $.showAlert({
+            title: "แจ้งเตือน",
+            body: "บันทึกข้อมูลเกิดข้อผิดพลาด",
+            onDispose: function () {
+                window.location.reload();
+            }
+        });
+    }
+}
