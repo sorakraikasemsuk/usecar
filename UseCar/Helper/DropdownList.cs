0be629cad743ada02971abde8c4f7fef6c52b92b
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-15 14:51:49 +0700

    add helper DropdownList

---
+++ UseCar/Helper/DropdownList.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UseCar.Models;
+
+namespace UseCar.Helper
+{
+    public class DropdownList
+    {
+        readonly UseCarDBContext context;
+        public DropdownList(UseCarDBContext context)
+        {
+            this.context = context;
+        }
+        public List<SelectListItem> DeaprtmentAll()
+        {
+            return (from a in context.department
+                    where a.isEnable
+                    select new SelectListItem
+                    {
+                        Value = a.departmentId.ToString(),
+                        Text = a.departmentName
+                    }).ToList();
+        }
+    }
+}
