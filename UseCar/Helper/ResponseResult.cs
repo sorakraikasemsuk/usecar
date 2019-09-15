4fe7e8541491db0bafa20006b8bfb15d421e26be
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-15 16:00:46 +0700

    add helper ResponseResult

---
+++ UseCar/Helper/ResponseResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseCar.Helper
+{
+    public class ResponseResult
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public dynamic data { get; set; }
+    }
+    public static class ResponseCode
+    {
+        public const int ok = 200;
+        public const int error = 500;
+    }
+}
