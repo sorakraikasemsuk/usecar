30ba372e804e0bb915a427840bfadac565337b47
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-16 16:51:38 +0700

    add helper resource

---
+++ UseCar/Helper/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseCar.Helper
+{
+    public static class Resource
+    {
+    }
+    public static class Permission
+    {
+        public const string view = "view";
+        public const string add = "add";
+        public const string edit = "edit";
+        public const string delete = "delete";
+    }
+}
