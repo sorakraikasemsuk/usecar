dc3fcc64ee5624777b90d829132a2b455e0a5e2d
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-16 16:51:47 +0700

    add session

--- UseCar/Helper/Session.cs
+++ UseCar/Helper/Session.cs
@@ -8,5 +8,8 @@ namespace UseCar.Helper
     public static class Session
     {
         public const string userId = "userId";
+        public const string firstName = "firstName";
+        public const string lastName = "lastName";
+        public const string menuPermission = "menuPermission";
     }
 }
