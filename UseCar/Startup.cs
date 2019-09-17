ef0750dd5807020f6b990a6996b48962e1cc5131
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-17 17:03:19 +0700

    inject CarSettingRepository

--- UseCar/Startup.cs
+++ UseCar/Startup.cs
@@ -62,6 +62,7 @@ namespace Car_Somchai
             services.AddTransient<UserManagementRepository>();
             services.AddTransient<PermissionManagementRepository>();
             services.AddTransient<ManageBranchRepository>();
+            services.AddTransient<CarSettingRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
