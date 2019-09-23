972b4482f13f9ab7999581c822fc3e43547a7412
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-23 15:56:24 +0700

    add SharedData for reuse data

---
+++ UseCar/Helper/SharedData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UseCar.Models;
+
+namespace UseCar.Helper
+{
+    public class SharedData
+    {
+        readonly UseCarDBContext context;
+        public SharedData(UseCarDBContext context)
+        {
+            this.context = context;
+        }
+        public List<SharedDataOptionViewModel> OptionData()
+        {
+            return (from a in context.option
+                    where a.isEnable
+                    select new SharedDataOptionViewModel
+                    {
+                        optionId = a.optionId,
+                        optionName = a.optionName
+                    }).ToList();
+        }
+    }
+    public class SharedDataOptionViewModel
+    {
+        public int optionId { get; set; }
+        public string optionName { get; set; }
+    }
+}
