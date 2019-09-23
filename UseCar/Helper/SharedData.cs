1923cc7e4f9910d668fddeb3619d2df3db9564e6
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-23 20:40:23 +0700

    เพิ่ม shared data vendor

--- UseCar/Helper/SharedData.cs
+++ UseCar/Helper/SharedData.cs
@@ -23,10 +23,32 @@ namespace UseCar.Helper
                         optionName = a.optionName
                     }).ToList();
         }
+        public SheardDataVendorViewModel VendorData(int vendorId)
+        {
+            return (from a in context.vendor
+                    where a.isEnable
+                    && a.vendorId == vendorId
+                    select new SheardDataVendorViewModel
+                    {
+                        vendorId = a.vendorId,
+                        vendorName = a.vendorName,
+                        vendorAddress = a.vendorAddress,
+                        vendorTel = a.vendorTel,
+                        vendorNumber = a.vendorNumber
+                    }).FirstOrDefault() ?? new SheardDataVendorViewModel();
+        }
     }
     public class SharedDataOptionViewModel
     {
         public int optionId { get; set; }
         public string optionName { get; set; }
+    }
+    public class SheardDataVendorViewModel
+    {
+        public int vendorId { get; set; }
+        public string vendorName { get; set; }
+        public string vendorAddress { get; set; }
+        public string vendorTel { get; set; }
+        public string vendorNumber { get; set; }
     }
 }
