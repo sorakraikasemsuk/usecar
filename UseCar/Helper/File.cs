915350636c140029d5be05fa612744168f337c6c
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-28 16:12:26 +0700

    set config upload

--- UseCar/Helper/File.cs
+++ UseCar/Helper/File.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,10 +11,15 @@ namespace UseCar.Helper
 {
     public class File
     {
+        readonly IConfiguration configuration;
+        public File(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public async Task Upload(IFormFile file,string folderName,string moduleName)
         {
             var uniqueFileName = file.FileName;
-            var uploads = Path.Combine("C:\\UseCar\\", folderName, moduleName);
+            var uploads = Path.Combine(configuration["Upload:Path"], folderName, moduleName);
             var filePath = Path.Combine(uploads, uniqueFileName);
 
             if (!Directory.Exists(uploads))
