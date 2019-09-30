a7a706860f1960add6fa59089efe5c7afa45c843
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-09-30 22:43:17 +0700

    add MaintenanceCarRepository

---
+++ UseCar/Repositories/MaintenanceCarRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UseCar.Models;
+
+namespace UseCar.Repositories
+{
+    public class MaintenanceCarRepository
+    {
+        readonly UseCarDBContext context;
+        readonly HttpContext httpContext;
+        public MaintenanceCarRepository(UseCarDBContext context,IHttpContextAccessor httpContext)
+        {
+            this.context = context;
+            this.httpContext = httpContext.HttpContext;
+        }
+    }
+}
