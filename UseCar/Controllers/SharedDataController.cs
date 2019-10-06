171b88a71ede6a7791c5a476d9fed6e804a8ab23
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-10-06 14:32:04 +0700

    add CarSearch

---
+++ UseCar/Controllers/SharedDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UseCar.Helper;
+
+namespace UseCar.Controllers
+{
+    public class SharedDataController : Controller
+    {
+        readonly SharedData shared;
+        public SharedDataController(SharedData shared)
+        {
+            this.shared = shared;
+        }
+        public JsonResult CarSearch(SearchCarFilter filter)
+        {
+            return Json(shared.CarSearch(filter));
+        }
+    }
+}
--- UseCar/Helper/SharedData.cs
+++ UseCar/Helper/SharedData.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using UseCar.Models;
@@ -9,9 +13,13 @@ namespace UseCar.Helper
     public class SharedData
     {
         readonly UseCarDBContext context;
-        public SharedData(UseCarDBContext context)
+        readonly IConfiguration configuration;
+        readonly FileManagement file;
+        public SharedData(UseCarDBContext context, IConfiguration configuration, FileManagement file)
         {
             this.context = context;
+            this.configuration = configuration;
+            this.file = file;
         }
         public List<SharedDataOptionViewModel> OptionData()
         {
@@ -37,6 +45,35 @@ namespace UseCar.Helper
                         vendorNumber = a.vendorNumber ?? ""
                     }).FirstOrDefault() ?? new SheardDataVendorViewModel();
         }
+        public List<SearchCarViewModel> CarSearch(SearchCarFilter filter)
+        {
+            using(var connection=new MySqlConnection(configuration.GetConnectionString("UseCarDBCOntext")))
+            {
+                var queryParameters = new DynamicParameters();
+                queryParameters.Add("@branchId", filter.branchId);
+                queryParameters.Add("@brandId", filter.brandId);
+                queryParameters.Add("@generationId", filter.generationId);
+                queryParameters.Add("@faceId", filter.faceId);
+                queryParameters.Add("@subfaceId", filter.subfaceId);
+                queryParameters.Add("@registerNumber", string.IsNullOrEmpty(filter.registerNumber) ? "" : filter.registerNumber);
+                var data = connection.Query<SearchCarViewModel>("st_getCarSearchList", queryParameters, commandType: CommandType.StoredProcedure);
+                return (from a in data
+                        select new SearchCarViewModel
+                        {
+                            carId = a.carId,
+                            brandId = a.brandId,
+                            brandName = a.brandName,
+                            generationId = a.generationId,
+                            generationName = a.generationName,
+                            faceId = a.faceId,
+                            faceName = a.faceName,
+                            subfaceId = a.subfaceId,
+                            subfaceName = a.subfaceName,
+                            image = file.GetImage($"{configuration["Upload:Path"]}{a.code}\\{MenuName.ReceiveCar}\\{a.image}"),
+                            registerNumber = a.registerNumber
+                        }).ToList();
+            }
+        }
     }
     public class SharedDataOptionViewModel
     {
@@ -50,5 +87,29 @@ namespace UseCar.Helper
         public string vendorAddress { get; set; }
         public string vendorTel { get; set; }
         public string vendorNumber { get; set; }
+    }
+    public class SearchCarViewModel
+    {
+        public int carId { get; set; }
+        public string code { get; set; }
+        public int brandId { get; set; }
+        public string brandName { get; set; }
+        public int generationId { get; set; }
+        public string generationName { get; set; }
+        public int faceId { get; set; }
+        public string faceName { get; set; }
+        public int subfaceId { get; set; }
+        public string subfaceName { get; set; }
+        public string image { get; set; }
+        public string registerNumber { get; set; }
+    }
+    public class SearchCarFilter
+    {
+        public int branchId { get; set; }
+        public int brandId { get; set; }
+        public int generationId { get; set; }
+        public int faceId { get; set; }
+        public int subfaceId { get; set; }
+        public string registerNumber { get; set; }
     }
 }
