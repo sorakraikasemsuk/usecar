660d0a2f51bb9810871249f73de8b43f144129b2
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-10-23 11:56:27 +0700

    inject CarRepository

--- UseCar/Startup.cs
+++ UseCar/Startup.cs
@@ -72,6 +72,7 @@ namespace Car_Somchai
             services.AddTransient<MaintenanceCarRepository>();
             services.AddTransient<CheckupSettingRepository>();
             services.AddTransient<CheckupCarRepository>();
+            services.AddTransient<CarRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
