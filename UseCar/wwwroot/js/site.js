454106ea5899c21cb4943f7105f6dbd3edb1a44f
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-10-23 14:38:57 +0700

    refactorcode

--- UseCar/wwwroot/js/site.js
+++ UseCar/wwwroot/js/site.js
@@ -27,3 +27,52 @@ function ExistDialog(text) {
         body: 'ข้อมูล <b>' + text + '</b> มีอยู่ในระบบแล้ว'
     });
 }
+//dropdown car data
+function BrandSelect() {
+    $('#generationId > option').not(':first').remove();
+    var _brandId = $('#brandId').val();
+    $.ajax({
+        url: '/SharedData/GetGenerationByBrandId',
+        type: 'GET',
+        data: {
+            brandId: _brandId
+        },
+        success: function (data) {
+            data.forEach(function (item) {
+                $("#generationId").append(new Option(item.text, item.value));
+            })
+        }
+    })
+}
+function GenerationSelect() {
+    $('#faceId > option').not(':first').remove();
+    var _generationId = $('#generationId').val();
+    $.ajax({
+        url: '/SharedData/GetFaceById',
+        type: 'GET',
+        data: {
+            generationId: _generationId
+        },
+        success: function (data) {
+            data.forEach(function (item) {
+                $("#faceId").append(new Option(item.text, item.value));
+            });
+        }
+    })
+}
+function FaceSelect() {
+    $('#subFaceId > option').not(':first').remove();
+    var _faceId = $('#faceId').val();
+    $.ajax({
+        url: '/SharedData/GetSubFaceByFaceId',
+        type: 'GET',
+        data: {
+            faceId: _faceId
+        },
+        success: function (data) {
+            data.forEach(function (item) {
+                $("#subFaceId").append(new Option(item.text, item.value));
+            });
+        }
+    })
+}
