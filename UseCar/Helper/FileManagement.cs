aa2ad43f2f16263e1bae128b29a62b8c3a1cff48
Sorakrai Kasemsuk <sorakrai.k@clicknext.com>
2019-10-27 13:33:29 +0700

    add GetImageByMenu

--- UseCar/Helper/FileManagement.cs
+++ UseCar/Helper/FileManagement.cs
@@ -6,15 +6,18 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using UseCar.Models;
 
 namespace UseCar.Helper
 {
     public class FileManagement
     {
         readonly IConfiguration configuration;
-        public FileManagement(IConfiguration configuration)
+        readonly UseCarDBContext context;
+        public FileManagement(IConfiguration configuration, UseCarDBContext context)
         {
             this.configuration = configuration;
+            this.context = context;
         }
         public async Task Upload(IFormFile file,string folderName,string moduleName)
         {
@@ -59,6 +62,36 @@ namespace UseCar.Helper
             }
             return "";
             
+        }
+        public string GetImageByMenu(int menuId,string code,string fileName,int refId)
+        {
+            string pathImage = "";
+            switch (menuId)
+            {
+                case MenuId.ReceiveCar:
+                    pathImage = $"{configuration["Upload:Path"]}{code}\\{MenuName.ReceiveCar}\\{fileName}";
+                    break;
+                case MenuId.CheckupCar:
+                    pathImage = $"{configuration["Upload:Path"]}{code}\\{MenuName.CheckupCar}\\{fileName}";
+                    break;
+                case MenuId.MaintenanceCar:
+                    string maCode = context.car_maintenance.FirstOrDefault(a => a.maintenanceId == refId).code;
+                    pathImage = $"{configuration["Upload:Path"]}{code}\\{MenuName.MaintenanceCar}\\{maCode}\\{fileName}";
+                    break;
+            }
+            if (File.Exists(pathImage))
+            {
+                using (var stream = new FileStream(pathImage, FileMode.Open))
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        stream.CopyTo(memoryStream);
+                        return "data:image/png;base64," + Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+            return "";
+
         }
     }
 }
